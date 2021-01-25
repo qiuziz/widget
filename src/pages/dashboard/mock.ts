ec4c09af6b99d87ea3bfb31a2298f493175a4d8b
qiuz <zhu_anly@163.com>
2021-01-25 20:19:36 +0800

    fix: path / end

--- src/pages/dashboard/mock.ts
+++ src/pages/dashboard/mock.ts
@@ -3,7 +3,7 @@ export const MENU_LIST = [
     id: 1,
     key: 1,
     title: 'React Image Map',
-    url: '/widget/react-image-map',
+    url: '/widget/react-image-map/',
     state: 1,
     parent_id: 0,
     oper_user_id: 0
